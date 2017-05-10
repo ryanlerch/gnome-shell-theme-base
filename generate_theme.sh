f450da6cf10a54d69d9d43e6e671a258dedd0305
Ryan Lerch <rlerch@redhat.com>
2017-05-10 13:51:13 +1000

    add basic build script, and a blank overrides file

---
+++ generate_theme.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf dist/ build/
+mkdir dist/ build/
+cp -r gnome-shell/data/theme/* build/
+cat scss/overrides.scss >> build/gnome-shell.scss
+sass build/gnome-shell.scss dist/gnome-shell.css
+cp build/*.png dist/
+cp build/*.svg dist/
