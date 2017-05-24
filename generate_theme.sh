937e522d8e128bb40bf0e400f75e38e4721ab017
Ryan Lerch <rlerch@redhat.com>
2017-05-24 13:11:04 +1000

    light variant

--- generate_theme.sh
+++ generate_theme.sh
@@ -3,7 +3,7 @@
 rm -rf dist/ build/
 mkdir dist/ build/
 cp -r gnome-shell/data/theme/* build/
-cat scss/overrides.scss >> build/gnome-shell.scss
+cp scss/overrides.scss build/gnome-shell.scss
 sass build/gnome-shell.scss dist/gnome-shell.css
 cp build/*.png dist/
 cp build/*.svg dist/
